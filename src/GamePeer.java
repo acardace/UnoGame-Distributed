bd7c43822728583e736cf518d176d532bc0f62e4
ichigo663 <ichigo663@gmail.com>
2016-02-08 18:05:16 +0100

    FTToken passing implemented, failure detector mechanism added, all done multithreaded

--- src/GamePeer.java
+++ src/GamePeer.java
@@ -1,24 +1,35 @@
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GamePeer implements RemotePeer{
+    public HashMap<Integer, RemotePeer> remotePeerHashMap;
+    public ReentrantLock ftTokenRecvLock;
+    public boolean ftTokenRecv;
+
     private boolean hasGameToken;
     private boolean hasFTToken;
     private int ID;
-    public HashMap<Integer, RemotePeer> remotePeerHashMap;
+    private Timer ftTimer;
+    private FTTokenPasserThread ftTokenPasserThread;
+
     private static final String RMI_OBJ_NAME = "RemotePeer";
+    private static final int FT_TIMEOUT = 3000; //in ms
+    private static final int FT_RING_DIRECTION = 1;
 
     public GamePeer(int id){
         this.ID = id;
         hasGameToken = false;
         remotePeerHashMap = new HashMap<>();
         initRMIServer();
-        //init fault tolerance thread
-
+        initFT();
     }
 
     public GamePeer(int id, boolean hasGameToken, boolean hasFTToken){
@@ -27,6 +38,14 @@ public class GamePeer implements RemotePeer{
         this.hasFTToken = hasFTToken;
         remotePeerHashMap = new HashMap<>();
         initRMIServer();
+        initFT();
+    }
+
+    private void initFT(){
+        ftTimer = new Timer();
+        ftTokenPasserThread = new FTTokenPasserThread();
+        ftTokenRecvLock = new ReentrantLock();
+        ftTokenRecv = false;
     }
 
     private void initRMIServer(){
@@ -44,37 +63,20 @@ public class GamePeer implements RemotePeer{
         }
     }
 
-    public void addRemotePeer(String addr){
-        try {
-            Registry registry = LocateRegistry.getRegistry(addr);
-            RemotePeer remotePeer = (RemotePeer) registry.lookup(RMI_OBJ_NAME);
-            remotePeerHashMap.put(remotePeer.getID(), remotePeer);
-        } catch (Exception e) {
-            System.err.println("addRemotePeer exception:");
-            e.printStackTrace();
-        }
+    public void startFTTokenPassing(){
+        ftTokenPasserThread.start();
+    }
+
+    public void addRemotePeer(String addr) throws NotBoundException, RemoteException{
+        Registry registry = LocateRegistry.getRegistry(addr);
+        RemotePeer remotePeer = (RemotePeer) registry.lookup(RMI_OBJ_NAME);
+        remotePeerHashMap.put(remotePeer.getID(), remotePeer);
     }
 
     public void sendGameToken(int peerID) throws RemoteException{
         if(hasGameToken){
             hasGameToken = false;
             remotePeerHashMap.get(peerID).getGameToken();
-        }
-    }
-
-    public void passFTToken(){
-        if(hasFTToken){
-            hasFTToken = false;
-            int nextPeer = getNextInRing();
-            if(nextPeer != -1) {
-                try {
-                    remotePeerHashMap.get(nextPeer).getFTToken();
-                }catch (RemoteException e){
-                    System.out.println("No other processes present in the ring");
-                }
-            }
-            else
-                System.out.println("No other processes present in the ring");
         }
     }
 
@@ -89,25 +91,95 @@ public class GamePeer implements RemotePeer{
     }
 
     public void getFTToken(){
-        hasFTToken = true;
-        try{
-            //Simulating processing of token
-            Thread.sleep(500);
-            passFTToken();
-            System.out.println("Token received");
-        }catch (InterruptedException e){System.out.println("Sleep interrupted");};
+        ftTokenRecvLock.lock();
+        ftTokenRecv = true;
+        ftTokenRecvLock.unlock();
+        try {
+            ftTokenPasserThread.lock.lock();
+            hasFTToken = true;
+            ftTokenPasserThread.recvdFTToken.signal();
+        }finally {
+            ftTokenPasserThread.lock.unlock();
+        }
+        System.out.println("Token received");
     }
 
-    //get the next peerID
+    //get the next peerID the ring
     //if it return -1 it means there are no neighbours
-    public int getNextInRing(){
+    public int getNextInRing(int direction){
         int peersNumber = remotePeerHashMap.size();
-        for(int i = getID()+UnoRules.getDirection(); i != getID(); i = (i+UnoRules.getDirection()) % peersNumber ){
+        for(int i = getID()+direction; i != getID(); i = (i+direction) % peersNumber ){
             if(remotePeerHashMap.containsKey(i))
                 return i;
         }
         return -1;
     }
 
+    public class FaultToleranceThread extends TimerTask{
+
+         public void run(){
+             ftTokenRecvLock.lock();
+             System.out.println("Timeout Elapsed");
+             if(ftTokenRecv) {
+                 //internal ACK
+                 ftTokenRecv = false;
+                 ftTokenRecvLock.unlock();
+                 System.out.println("FT TOKEN OK");
+             }
+             else {
+                 ftTokenRecvLock.unlock();
+                 System.out.println("FT_Thread: Failure detected, token not received in "+FT_TIMEOUT+"ms");
+             }
+         }
+    }
+
+    public class FTTokenPasserThread extends Thread{
+        public ReentrantLock lock;
+        public Condition recvdFTToken;
+
+        public FTTokenPasserThread(){
+            lock = new ReentrantLock();
+            recvdFTToken = lock.newCondition();
+        }
+
+        public void run(){
+            while(true){
+                try {
+                    lock.lock();
+                    while (!hasFTToken)
+                        recvdFTToken.await();
+                    //DO PASSING
+                    try{
+                        //Simulating processing of token
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){System.out.println("Sleep interrupted");}
+                    passFTToken();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    lock.unlock();
+                }
+            }
+        }
+
+        private void passFTToken(){
+            hasFTToken = false;
+            int nextPeer = getNextInRing(FT_RING_DIRECTION);
+            while(nextPeer != -1 ){
+                try {
+                    //Pass token
+                    remotePeerHashMap.get(nextPeer).getFTToken();
+                    System.out.println("Token Sent");
+                    //Launch Fault tolerance timeout
+                    ftTimer.schedule(new FaultToleranceThread(), FT_TIMEOUT);
+                    break;
+                }catch (RemoteException e){
+                    System.out.println("Communication failed, removing host from hashMap");
+                    remotePeerHashMap.remove(nextPeer);
+                }
+                nextPeer = getNextInRing(FT_RING_DIRECTION);
+            }
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
-import java.rmi.RemoteException;
-import java.util.Iterator;
 import java.util.Scanner;
 
 //TESTING
 public class Main {
     public static void main(String[] args){
+        //Main peerID remotePeer
         GamePeer p1 = new GamePeer(Integer.parseInt(args[0]),
                 ( (Integer.parseInt(args[0]) == 0 ) ? true : false), ( (Integer.parseInt(args[0]) == 0 ) ? true : false));
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input something to go on");
         scanner.next();
         System.out.println("Adding remote peer "+args[1]);
-        p1.addRemotePeer(args[1]);
-        System.out.println(args[1]+" added!");
         try {
-            Iterator<Integer> iterator = p1.remotePeerHashMap.keySet().iterator();
-            int remoteID = p1.remotePeerHashMap.get(iterator.next()).getID();
-            System.out.println("Remote Peer ID: "+remoteID);
-            Thread.sleep(3000);
-            p1.sendGameToken(remoteID);
-            System.out.println("No token no more");
-            System.out.println("Next in ring: "+p1.remotePeerHashMap.get(p1.getNextInRing()).getID());
-            p1.passFTToken();
-        }catch (RemoteException e){e.printStackTrace();}
-        catch (InterruptedException e){e.printStackTrace();}
+            p1.addRemotePeer(args[1]);
+            System.out.println(args[1] + " added!");
+            p1.startFTTokenPassing();
+        }catch (Exception e){
+            System.out.println("Connection refused");
+        }
     }
 }
