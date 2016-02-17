4730ca5ea9e37d4a4784df06c365a69138fba342
Michele Cucchi <cucchi@cs.unibo.it>
2016-02-17 16:24:30 +0100

    added lock and timer to start game

--- src/GameRegistration.java
+++ src/GameRegistration.java
@@ -5,7 +5,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by michele on 16/02/16.
@@ -14,12 +17,21 @@ public class GameRegistration implements RemoteRegistration {
     private static final String RMI_REG_OBJ_NAME = "RegistrationService";
     private static final String RMI_OBJ_NAME = "RemotePeer";
     private static final int MAX_PLAYERS = 8;
+    private static final int MIN_START_PLAYERS = 3;
+    private static final int START_GAME_TIMEOUT = 300000; // milliseconds -> 5 minuti
 
     private HashMap<Integer, PlayerReady> playersHashMap;
     private int playersReadyCounter;
     private int playersCounter;
+    private Timer startGameTimer;
+
+    public ReentrantLock syncPlayersLock;
+    public Condition syncPlayersCondition;
 
     public GameRegistration() {
+        syncPlayersLock = new ReentrantLock();
+        syncPlayersCondition = syncPlayersLock.newCondition();
+
         playersReadyCounter = 0;
         playersCounter = 0;
         playersHashMap = new HashMap<>();
@@ -34,44 +46,92 @@ public class GameRegistration implements RemoteRegistration {
         return playersCounter;
     }
 
-    private PlayersRegistrationData registerPlayer(String addr) throws  RemoteException, NotBoundException {
+    private void setGameStartTimer(int timeout) {
+        if(startGameTimer == null) {
+            startGameTimer = new Timer();
+            startGameTimer.schedule(new startGameThread(), timeout);
+        }
+    }
+
+    private void waitGameStart() {
+       while(true) {
+           try {
+               syncPlayersLock.lock();
+               syncPlayersCondition.await();
+           } catch (InterruptedException e) {
+               // do nothing
+           } finally {
+               syncPlayersLock.unlock();
+           }
+       }
+    }
+
+    private void gameStart() {
+        syncPlayersLock.lock();
+        syncPlayersCondition.signalAll();
+        syncPlayersLock.unlock();
+    }
+
+    private int[] registerPlayer(int id, String addr) throws  RemoteException, NotBoundException {
         // insert player in "play room"
         Registry registry = LocateRegistry.getRegistry(addr);
         RemotePeer remotePeer = (RemotePeer) registry.lookup(RMI_OBJ_NAME);
-        PlayersRegistrationData newPlayerDataOut = null;
-        int newPlayerID = newPlayerID();
 
-        if(newPlayerID > 0) {
-            playersHashMap.put(newPlayerID, new PlayerReady(remotePeer, false));
-
-            Integer[] playersId = (Integer[]) playersHashMap.keySet().toArray();
-            int[] playersIntID = new int[playersId.length];
-
-            for(int i=0;i<playersId.length;i++) {
-                if (playersId != null)
-                    playersIntID[i] = playersId[i].intValue();
-            }
-
-            newPlayerDataOut = new PlayersRegistrationData(newPlayerID, playersIntID);
-        }
-        else {
-            newPlayerDataOut = new PlayersRegistrationData(newPlayerID, null);
+        if(id > 0) {
+            playersHashMap.put(id, new PlayerReady(remotePeer, false));
         }
 
-        // todo send broadcast notice to other players
-        return newPlayerDataOut;
+        Integer[] playersId = (Integer[]) playersHashMap.keySet().toArray();
+        int[] playersIntID = new int[playersId.length];
+
+        for(int i=0;i<playersId.length;i++) {
+            if (playersId != null)
+                playersIntID[i] = playersId[i].intValue();
+        }
+
+        return playersIntID;
     }
 
     @Override
-    public PlayersRegistrationData playerRegistration(String addr) throws RemoteException, NotBoundException {
-        PlayersRegistrationData newPlayerDataOut = registerPlayer(addr);
+    public int getNewPlayerID() throws RemoteException {
+        return newPlayerID();
+    }
 
-        return newPlayerDataOut;
+    @Override
+    public int[] getPlayersID() throws RemoteException {
+        Integer[] playersId = (Integer[]) playersHashMap.keySet().toArray();
+        int[] playersIntID = new int[playersId.length];
+
+        for(int i=0;i<playersId.length;i++) {
+            if (playersId != null)
+                playersIntID[i] = playersId[i].intValue();
+        }
+
+        return playersIntID;
+    }
+
+    @Override
+    public int[] playerRegistration(int id, String addr) throws RemoteException, NotBoundException {
+        return registerPlayer(id, addr);
     }
 
     @Override
     public void playerReady(int playerID) throws RemoteException {
-        
+        playersHashMap.get(playerID).playerReady = true;
+        playersReadyCounter++;
+
+        if(playersReadyCounter == playersCounter) {
+            // todo sblocca tutti e lancia il gioco
+            gameStart();
+        }
+        else {
+            if(playersReadyCounter >= MIN_START_PLAYERS)
+            // todo controlla se timeout già partito else fallo partire
+                setGameStartTimer(START_GAME_TIMEOUT);
+
+            // todo blocca esecuzione in attesa dello start
+            waitGameStart();
+        }
     }
 
     public static void main(String[] args) {
@@ -92,5 +152,12 @@ public class GameRegistration implements RemoteRegistration {
             e.printStackTrace();
         }
 
+    }
+
+    public class startGameThread extends TimerTask {
+        public void run() {
+            // thread che esegue allo scadere del timeout settato dopo il ready del 3th giocatore
+            gameStart();
+        }
     }
 }
--- src/RemoteRegistration.java
+++ src/RemoteRegistration.java
@@ -6,6 +6,15 @@ import java.rmi.RemoteException;
  * Created by michele on 16/02/16.
  */
 public interface RemoteRegistration extends Remote {
-    public PlayersRegistrationData playerRegistration(String addr) throws RemoteException, NotBoundException;
+    // first function return the new player id
+    public int getNewPlayerID() throws RemoteException;
+
+    // function to register new player in the registration server
+    public int[] playerRegistration(int id, String addr) throws RemoteException, NotBoundException;
+
+    // returns an array with all players id
+    public int[] getPlayersID() throws RemoteException;
+
+    // set player status to ready, lock waiting for game start
     public void playerReady(int playerID) throws RemoteException;
 }
