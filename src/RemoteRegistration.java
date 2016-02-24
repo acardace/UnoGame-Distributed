a2de6abea2b214263ee4e724718071bad313c91f
Antonio Cardace <anto.cardace@gmail.com>
2016-02-24 19:15:31 +0100

    fixed bug in player registration

--- src/RemoteRegistration.java
+++ src/RemoteRegistration.java
@@ -10,7 +10,7 @@ public interface RemoteRegistration extends Remote {
     public int getNewPlayerID() throws RemoteException;
 
     // function to register new player in the registration server
-    public PlayerReady[] playerRegistration(int id, String addr) throws RemoteException, NotBoundException;
+    public PlayerReady[] playerRegistration(int id, String playerAddr) throws RemoteException, NotBoundException;
 
     // returns an array with all players id
     public int[] getPlayersID() throws RemoteException;
